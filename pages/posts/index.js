f14bc83ed90a431f5ea10f60b57b37eece33f53c
Tanja Nobel <tanja@nobel.swiss>
2022-07-11 20:02:43 +0200

    Add the all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,39 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs3',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs4',
+        title: 'Getting Started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2022-02-10',
+    },
+];
+
 const AllPostsPage = () => {
     return (
-        <div>
-
-        </div>
+        <AllPosts posts={DUMMY_POSTS} />
     );
 };
 
