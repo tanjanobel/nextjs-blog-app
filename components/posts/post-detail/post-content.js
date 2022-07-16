151b65c387f38632abc200c8040ac282b3530cf1
Tanja Nobel <tanja@nobel.swiss>
2022-07-16 11:07:43 +0200

    Render code snippets from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css"
@@ -30,6 +32,18 @@ const PostContent = (props) => {
 
             return <p>{paragraph.children}</p>;
         },
+
+        code(code) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter
+                    style={atomDark}
+                    language={language}
+                    children={children}
+                />
+            );
+        }
     };
 
     return (
