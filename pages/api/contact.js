6809b671e7afd6be2bf5bd9f236e56ef30c88c84
Tanja Nobel <tanja@nobel.swiss>
2022-07-17 13:23:02 +0200

    Store messages with mongodb in a database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -14,14 +16,35 @@ const handler = (req, res) => {
             return;
         }
 
-        // Store it in a database
         const newMessage = {
             email,
             name,
             message
         };
 
-        console.log(newMessage);
+        let client;
+
+        try {
+            client = await MongoClient.connect(
+                `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.trhh4.mongodb.net/nextjs-blog-app?retryWrites=true&w=majority`
+            );
+        } catch (error) {
+            res.status(500).json({message: 'Could not connect to database.'});
+            return;
+        }
+
+        const db = client.db();
+
+        try {
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({message: 'Storing message failed!'});
+            return;
+        }
+
+        client.close();
 
         res
             .status(201)
