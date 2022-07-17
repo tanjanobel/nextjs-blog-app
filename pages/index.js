c0e6afaf4da8ff5c1ef269adc7cdf5586bb7ed5e
Tanja Nobel <tanja@nobel.swiss>
2022-07-17 14:15:04 +0200

    Add head data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -5,6 +7,10 @@ import { getFeaturedPosts } from "../lib/posts-util";
 const HomePage = (props) => {
     return (
         <>
+            <Head>
+                <title>Zoa's Blog</title>
+                <meta name="description" content="I post about programming and web development." />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts} />
         </>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
 
 const AllPostsPage = (props) => {
     return (
-        <AllPosts posts={props.posts} />
+        <>
+            <Head>
+                <title>All Posts</title>
+                <description name="description" content="A list of all programming-related tutorials and posts!" />
+            </Head>
+            <AllPosts posts={props.posts} />
+        </>
     );
 };
 
 export const getStaticProps = () => {
-  const allPosts = getAllPosts();
+    const allPosts = getAllPosts();
 
-  return {
-      props: {
-          posts: allPosts
-      }
-  }
+    return {
+        props: {
+            posts: allPosts
+        }
+    }
 };
 
 export default AllPostsPage;
